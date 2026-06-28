a94f077f95d6ec3373a9823dc0287e30a58597c9
Sirish Chandra Yellanki <ysirishchandra@gmail.com>
2026-06-28 15:28:45 +0530

    fix: remove manual Access-Control-Allow-Origin from chatStream controller to prevent CORS conflicts

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -103,7 +103,6 @@ const chatStream = async (req, res, next) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
-    res.setHeader('Access-Control-Allow-Origin', '*');
     res.flushHeaders();
 
     // 5. Stream tokens
